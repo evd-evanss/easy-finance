f1b447509b673de2bf9e14d68216ca034d24139e
Evandro <evandro.ribeiro@mobile2you.com.br>
2021-06-12 16:25:13 -0300

    (feat/design-system)::Design System Created

---
+++ app/src/main/java/com/sugarspoon/easyfinance/base/BaseActivity.kt
@@ -0,0 +1,61 @@
+package com.sugarspoon.easyfinance.base
+
+import android.content.Context
+import android.content.pm.ActivityInfo
+import android.os.Bundle
+import android.view.MenuItem
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.viewbinding.ViewBinding
+import com.sugarspoon.easyfinance.R
+
+abstract class BaseActivity<Binding : ViewBinding> : AppCompatActivity() {
+
+    val context: Context
+        get() = this
+    lateinit var binding: Binding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        requestedOrientation = ActivityInfo.SCREEN_ORIENTATION_PORTRAIT
+        binding = getViewBinding()
+        setContentView(binding.root)
+    }
+
+    abstract fun getViewBinding(): Binding
+
+    private fun setToolbar(title: String) {
+        val toolbar = findViewById<androidx.appcompat.widget.Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+        setTitle(title)
+    }
+
+    fun setToolbar(
+        title: String = "",
+        displayHomeAsUpEnabled: Boolean = false
+    ) {
+        setToolbar(title)
+        supportActionBar?.setDisplayHomeAsUpEnabled(displayHomeAsUpEnabled)
+        supportActionBar?.setHomeAsUpIndicator(R.drawable.ic_back)
+        setTitle(title)
+    }
+
+    //MENU METHODS
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            android.R.id.home -> {
+                //this is used because when user hits home button the previous view is reconstructed
+                //and when back button (at navbar) is pressed this doesn't happen,
+                //so this makes the previous view never reconstructed when home is hit.
+                onBackPressed()
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    companion object {
+        private const val EMPTY = ""
+    }
+}
